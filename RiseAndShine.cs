e1f8e6377691783eb812d4da3239f793a4fa83f4
Yoshify <xYoshify@gmail.com>
2016-03-07 03:32:46 +1000

    Update
    v1.1
    -No longer dependent on the TrainerMod included with SMAPI.
    
    -Fixed bug where the time wasn't set on the first day of a new season.
    
    -Added new feature - Wake up at different times on weekends (If you wish
    to sleep in), disabled by default, changed in the config.

--- RiseAndShine.cs
+++ RiseAndShine.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
+using StardewValley;
 using StardewModdingAPI;
+using StardewModdingAPI.Events;
 
 namespace RiseAndShineMod
 {
@@ -18,7 +20,7 @@ namespace RiseAndShineMod
 
         public override string Version
         {
-            get { return "1.0"; }
+            get { return "1.1"; }
         }
 
         public override string Description
@@ -27,12 +29,15 @@ namespace RiseAndShineMod
         }
 
         public int wakeUpTime = 900; //Default wakeup time is 6AM (600).
+        public int weekendTime = 900; //Wake up at the same time on the weekends.
 
-        public override void Entry()
+        public override void Entry(params object[] objects)
         {
-            Console.WriteLine("Rise and Shine Mod has loaded!");
+            Console.WriteLine("Rise and Shine Mod loading...");
             DoLoadPlayerConfig();
-            Events.DayOfMonthChanged += SetTime;
+            TimeEvents.DayOfMonthChanged += SetTime;
+            TimeEvents.SeasonOfYearChanged += SetTime;
+            Console.WriteLine("Rise and Shine Mod has loaded!");
         }
 
         void DoLoadPlayerConfig()
@@ -51,22 +56,49 @@ namespace RiseAndShineMod
 
                 StreamWriter configWriter = new StreamWriter(configFilePath);
                 configWriter.WriteLine("WakeUpAt=900");
+                configWriter.WriteLine("WeekendWakeUpAt=900");
                 configWriter.Close();
 
                 configReader = File.OpenText(configFilePath);
             }
 
             string currLine = configReader.ReadLine();
-            bool couldParse = int.TryParse(currLine.Split('=')[1], out wakeUpTime);
+            bool couldParseA = int.TryParse(currLine.Split('=')[1], out wakeUpTime);
+            currLine = configReader.ReadLine();
+            bool couldParseB = int.TryParse(currLine.Split('=')[1], out weekendTime);
 
-            if (!couldParse) Console.WriteLine("Couldn't parse Rise and Shine config! Using default values.");
+            if (!couldParseA || !couldParseB)
+            {
+                Console.WriteLine("Couldn't parse Rise and Shine config! Using default values.");
+                configReader.Close();
+                return;
+            }
+                
+            if(wakeUpTime < 600 || weekendTime < 600)
+            {
+                wakeUpTime = wakeUpTime < 600 ? 600 : wakeUpTime;
+                weekendTime = weekendTime < 600 ? 600 : weekendTime;
+                Console.WriteLine("You cannot wake up earlier than 6am. Sorry!");
+            }
+            else if(wakeUpTime > 2600 || weekendTime > 2600)
+            {
+                wakeUpTime = wakeUpTime > 2600 ? 2600 : wakeUpTime;
+                weekendTime = weekendTime > 2600 ? 2600 : weekendTime;
+                Console.WriteLine("You cannot wake up later than 2am the next day (I don't know why you'd want to do this). Sorry!");
+            }
 
             configReader.Close();
         }
 
-        void SetTime(int T)
+        void SetTime(object sender, EventArgs e)
         {
-            Command.CallCommand("world_settime " + wakeUpTime);
+            string currDay = Game1.shortDayNameFromDayOfSeason(Game1.dayOfMonth);
+            bool shouldUseWeekendTime = (currDay == "Sat" || currDay == "Sun") ? true : false;
+
+            if (shouldUseWeekendTime)
+                Game1.timeOfDay = weekendTime;
+            else
+                Game1.timeOfDay = wakeUpTime;
         }
     }
 }
